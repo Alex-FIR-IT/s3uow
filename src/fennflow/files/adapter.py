85073aa5d9cf8efc04276a472f7c21f8315ddf5d
admin <osintfir@gmail.com>
2026-05-03 20:05:46 +0300

    refactor: sort imports

--- src/fennflow/files/adapter.py
+++ src/fennflow/files/adapter.py
@@ -1,6 +1,7 @@
 from typing import Annotated
-from pydantic import TypeAdapter, Field
+
+from pydantic import Field, TypeAdapter
 
 from .types import Media
 
-media_adapter = TypeAdapter(Annotated[Media, Field(discriminator='kind')])
+media_adapter = TypeAdapter(Annotated[Media, Field(discriminator="kind")])
