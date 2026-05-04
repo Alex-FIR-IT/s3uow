e55c3dcdca0218d05d2234c4bcc4097911926f2f
admin <osintfir@gmail.com>
2026-05-04 10:37:15 +0300

    chore: append InMemoryConnectorConfig into type_alias

--- src/fennflow/connectors/types/config.py
+++ src/fennflow/connectors/types/config.py
@@ -1,5 +1,6 @@
 from typing import TypeAlias
 
+from fennflow.connectors.in_memory import InMemoryConnectorConfig
 from fennflow.connectors.s3 import S3ConnectorConfig
 
-ConnectorConfig: TypeAlias = S3ConnectorConfig
+ConnectorConfig: TypeAlias = S3ConnectorConfig | InMemoryConnectorConfig
