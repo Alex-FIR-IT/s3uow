76566dbd25f81040efb3cac48568e5ea532fb056
admin <osintfir@gmail.com>
2026-05-06 22:55:46 +0300

    refactor: fix annotation (mypy)

--- src/fennflow/backends/abstract/config.py
+++ src/fennflow/backends/abstract/config.py
@@ -1,10 +1,12 @@
 from pydantic import BaseModel, Field
 
+from fennflow._new_types import Namespace
+
 
 class AbstractBackendConfig(BaseModel):
     """Base configuration for all FennFlow backends."""
 
-    namespace: str = Field(
+    namespace: Namespace = Field(
         default="fennflow_backend",
         description="Namespace of backend. "
         "Useful to distinguish between backends for different file storages.",
