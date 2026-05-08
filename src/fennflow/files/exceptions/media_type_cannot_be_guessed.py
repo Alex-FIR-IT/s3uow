6db13bcedea9a19a2901f84a19e4d85493ff43e9
admin <osintfir@gmail.com>
2026-05-08 14:38:56 +0300

    chore: adjust msg depending on whether extension is present in filename or not

--- src/fennflow/files/exceptions/media_type_cannot_be_guessed.py
+++ src/fennflow/files/exceptions/media_type_cannot_be_guessed.py
@@ -1,10 +1,21 @@
+import os.path
+
+
 class MediaTypeCannotBeGuessed(Exception):
     """Raised when the media type cannot be guessed from the filename."""
 
     def __init__(self, filename: str):
-        msg = (
-            f"Cannot guess media type for {filename=}. "
-            f"Please, specify media_type explicitly."
-        )
+        ext = os.path.splitext(filename)[1]
+
+        if ext:
+            msg = (
+                f"Cannot guess media type for {filename=}. "
+                f"Please, specify media_type explicitly."
+            )
+        else:
+            msg = (
+                "Cannot guess media type. "
+                "Please, specify media_type or filename with extension explicitly."
+            )
         super().__init__(msg)
         self.filename = filename
