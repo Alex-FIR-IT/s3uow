74621cae10ac755b97e06565ef16c8053ebe25be
admin <osintfir@gmail.com>
2026-05-09 02:23:25 +0300

    refactor: import overload from typing

--- src/fennflow/repositories/fields/base.py
+++ src/fennflow/repositories/fields/base.py
@@ -1,8 +1,6 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Generic, TypedDict, TypeVar
-
-from typing_extensions import overload
+from typing import TYPE_CHECKING, Generic, TypedDict, TypeVar, overload
 
 if TYPE_CHECKING:
     from fennflow._new_types import Namespace
