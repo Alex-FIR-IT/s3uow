1da87f30246465ae1cf8483824f4995aed4f6179
admin <osintfir@gmail.com>
2026-05-13 21:40:16 +0300

    fix: clear context of operation in clear_session for InMemoryBackend to avoid storing files in memory

---
+++ tests/backends/in_memory/test_clear_session.py
@@ -0,0 +1,46 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+import pytest
+
+from fennflow._operations.context.abstract import BaseContext
+from fennflow.files import TextContent
+
+if TYPE_CHECKING:
+    from fennflow.backends import InMemoryBackend
+
+
+@pytest.mark.asyncio
+async def test_operation_context_is_cleared_with_commit(uow_cls):
+    async with uow_cls() as uow:
+        for i in range(5):
+            await uow.user_files.at("folder1/").put(
+                TextContent.from_content(f"file{i}")
+            )
+
+        await uow.commit()
+
+        assert len(await uow.backend.list_pending()) == 0
+
+        backend: InMemoryBackend = uow.backend
+
+        for operation in backend.scoped_storage.values():
+            assert type(operation.context) is BaseContext
+
+
+@pytest.mark.asyncio
+async def test_operation_context_is_cleared_in_new_session(uow_cls):
+    async with uow_cls() as uow:
+        for i in range(5):
+            await uow.user_files.at("folder1/").put(
+                TextContent.from_content(f"file{i}")
+            )
+
+    async with uow_cls() as uow:
+        assert len(await uow.backend.list_pending()) == 0
+
+        backend: InMemoryBackend = uow.backend
+
+        for operation in backend.scoped_storage.values():
+            assert type(operation.context) is BaseContext
