3a48d75489e4c5538a82f53ebffa490b17da6aa9
admin <osintfir@gmail.com>
2026-05-14 20:56:10 +0300

    docs: add docstrings

--- src/fennflow/reconciler/config.py
+++ src/fennflow/reconciler/config.py
@@ -5,6 +5,8 @@ from fennflow.reconciler.enums import ReconcileFrequencyEnum, ReconcileStrategyE
 
 @dataclass(slots=True, frozen=True)
 class ReconcileConfig:
+    """Configuration for the reconciler."""
+
     frequency: ReconcileFrequencyEnum = ReconcileFrequencyEnum.ON_START_APP
     strategy: ReconcileStrategyEnum = ReconcileStrategyEnum.FILL_IF_EMPTY
     batch_size: int = 1000
