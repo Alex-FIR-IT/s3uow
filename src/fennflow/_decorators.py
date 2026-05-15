6a5bbef98a019da04b9ae1467287d68ae0b916f2
admin <osintfir@gmail.com>
2026-05-15 22:05:20 +0300

    chore: make reraise_with take predicate as CatchType as well

--- src/fennflow/_decorators.py
+++ src/fennflow/_decorators.py
@@ -3,7 +3,9 @@ import functools
 from collections.abc import Callable
 from typing import Any, TypeAlias
 
-CatchType: TypeAlias = type[Exception] | tuple[type[Exception], ...]
+CatchType: TypeAlias = (
+    type[Exception] | tuple[type[Exception], ...] | Callable[[Exception], bool]
+)
 
 
 def reraise_with(
@@ -12,19 +14,29 @@ def reraise_with(
 ) -> Callable[[Callable[..., Any]], Callable[..., Any]]:
     def decorator(func: Callable[..., Any]) -> Callable[..., Any]:
 
+        def _reraise_exception_if_match(e: Exception) -> None:
+            if isinstance(catch, (type, tuple)):
+                if isinstance(e, catch):
+                    raise exception from e
+
+            elif catch(e):
+                raise exception from e
+
         @functools.wraps(func)
         async def async_wrapper(*args: Any, **kwargs: Any) -> Any:
             try:
                 return await func(*args, **kwargs)
-            except catch as e:
-                raise exception from e
+            except Exception as e:
+                _reraise_exception_if_match(e)
+                raise
 
         @functools.wraps(func)
         def sync_wrapper(*args: Any, **kwargs: Any) -> Any:
             try:
                 return func(*args, **kwargs)
-            except catch as e:
-                raise exception from e
+            except Exception as e:
+                _reraise_exception_if_match(e)
+                raise
 
         if asyncio.iscoroutinefunction(func):
             return async_wrapper
